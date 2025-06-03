0709a517025631e9d16b35ff94836bdb4bce1a38
Anonfedora <musaeleazar090@gmail.com>
2025-06-03 22:22:32 +0100

    test: Deffered Settlement contract

---
+++ contracts/deferred-settlement/contracts/deferred-settlement/src/admin.rs
@@ -0,0 +1,13 @@
+use crate::types::{DataKey};
+use soroban_sdk::{Address, Env};
+
+pub fn initialize(env: Env, admin: Address) {
+    admin.require_auth();
+    env.storage().instance().set(&DataKey::Admin, &admin);
+    env.storage().instance().set(&DataKey::TotalTransactions, &0u128);
+}
+
+pub fn is_admin(env: &Env, address: &Address) -> bool {
+    let admin: Address = env.storage().instance().get(&DataKey::Admin).unwrap();
+    admin == *address
+}
